79bfd4a4f6cf6af1bceec745d842877d74bba933
oskar-anderson <andersonkarloskar@gmail.com>
2022-10-22 19:12:05 +0300

    proposed

--- wwwroot/ts/Draw.ts
+++ wwwroot/ts/Draw.ts
@@ -1,6 +1,7 @@
 import { Viewport } from "pixi-viewport";
 import * as PIXI from "pixi.js";
 import { Rectangle } from "pixi.js";
+import { Table } from "./Table";
 
 export class Draw {
 
@@ -8,26 +9,36 @@ export class Draw {
     app: PIXI.Application = null;
     // @ts-ignore: Object is possibly 'null'.
     viewport: Viewport = null;
+    screenDrawArea: string[] = [];
+    fontCharSizeWidth = 14;
+    fontCharSizeHeight = 14;
 
     constructor() {
 
     }
 
-    getScreensize() {
-        if (this.viewport === null) {
-            return new Rectangle(0, 0, 1280, 720);
-        }
-        return new Rectangle(Math.floor(this.viewport.x), Math.floor(this.viewport.y), 1280, 720);
+    getScreenSize() {
+        return new Rectangle(Math.floor(this.viewport.x), Math.floor(this.viewport.y), this.viewport.width, this.viewport.height);
     }
 
-    async init(drawArea: string[], width: number, height: number, fontSize = 14) {
-        let screen = this.getScreensize();
+    getCharGridSize() {
+        let screenSize = this.getScreenSize();
+        return new Rectangle(
+            Math.floor(screenSize.x / this.fontCharSizeWidth),
+            Math.floor(screenSize.y / this.fontCharSizeHeight),
+            Math.floor(screenSize.width / this.fontCharSizeWidth),
+            Math.floor(screenSize.height / this.fontCharSizeHeight), 
+            );
+    }
+
+    async init(screenSizeWidth: number, screenSizeHeight: number) {
+        let screenSize = new Rectangle(0, 0, screenSizeWidth, screenSizeHeight);
         let maxWorldSizeToZoomOutRatio = 1;
         let minZoomOut = 4;
         let maxZoomIn = 2;
         this.app = new PIXI.Application({
-            width: screen.width,
-            height: screen.height,
+            width: screenSize.width,
+            height: screenSize.height,
             backgroundColor: 0xAAAAAA
         });
 
@@ -36,33 +47,31 @@ export class Draw {
         this.app.loader.add('../wwwroot/font/ps2p/14xml/ps2p-14.fnt');
 
         this.viewport = new Viewport({
-            screenHeight: screen.height,
-            screenWidth:  screen.width,
-            worldHeight:  screen.height * minZoomOut * maxWorldSizeToZoomOutRatio,
-            worldWidth:   screen.width * minZoomOut * maxWorldSizeToZoomOutRatio,
+            screenHeight: screenSize.height,
+            screenWidth:  screenSize.width,
+            worldHeight:  screenSize.height * minZoomOut * maxWorldSizeToZoomOutRatio,
+            worldWidth:   screenSize.width * minZoomOut * maxWorldSizeToZoomOutRatio,
 
             interaction: this.app.renderer.plugins.interaction
         });
+        console.log(`init viewport - X: ${this.viewport.x}, Y: ${this.viewport.height}, width: ${this.viewport.width}, height: ${this.viewport.height}`)
+        console.log(JSON.parse(JSON.stringify(this.viewport.hitArea)));
+        console.log(this.viewport);
 
         this.app.stage.addChild(this.viewport);
         
         // activate plugins
         this.viewport
             .drag().clamp({ 
-                left:  -screen.width * minZoomOut * maxWorldSizeToZoomOutRatio + screen.width,
-                top:   -screen.height * minZoomOut * maxWorldSizeToZoomOutRatio + screen.height,
-                right:  screen.width * minZoomOut,
-                bottom: screen.height * minZoomOut,
+                left:  -screenSize.width * minZoomOut * maxWorldSizeToZoomOutRatio + screenSize.width,
+                top:   -screenSize.height * minZoomOut * maxWorldSizeToZoomOutRatio + screenSize.height,
+                right:  screenSize.width * minZoomOut,
+                bottom: screenSize.height * minZoomOut,
             })
             .wheel().clampZoom({ 
                 maxScale: maxZoomIn, 
                 minScale: 1/minZoomOut
             });
-
-        let sprite = this.viewport.addChild(new PIXI.Sprite(PIXI.Texture.WHITE))
-        sprite.tint = 0xff0000
-        sprite.width = sprite.height = 100
-        sprite.position.set(1600, 100)
 
         await new Promise<void>((resolve, reject) => {
             this.app.loader.load();
@@ -79,29 +88,100 @@ export class Draw {
         
     }
 
-    initScreen(drawArea: string[], width: number, height: number, fontSize = 14) {
-        for (let y = 0; y < height; y++) {
-            for (let x = 0; x < width; x++) {
-                let tile = drawArea[y * width + x];
+    initScreen() {
+        let charGridSize = this.getCharGridSize();
+        for (let y = 0; y < charGridSize.height; y++) {
+            for (let x = 0; x < charGridSize.width; x++) {
+                this.screenDrawArea.push(' ');
+            }
+        }
+        for (let y = 0; y < charGridSize.height; y++) {
+            for (let x = 0; x < charGridSize.width; x++) {
+                let tile = this.screenDrawArea[y * charGridSize.width + x];
                 let bitmapText = new PIXI.BitmapText(tile,
                     {
                         fontName: "Press Start 2P",
-                        fontSize: fontSize,
                         align: 'right',
                         tint: 0x008000
                     });
-                bitmapText.x = x * fontSize;
-                bitmapText.y = y * fontSize;
+                bitmapText.x = x * this.fontCharSizeWidth;
+                bitmapText.y = y * this.fontCharSizeHeight;
                 this.viewport.addChild(bitmapText)
             }
         }
     }
 
-    render(drawArea: string[], width: number, height: number) {
-        for (let y = 0; y < height; y++) {
-            for (let x = 0; x < width; x++) {
-                (this.viewport.children[y * width + x] as PIXI.Text).text = drawArea[y * width + x]
+    render() {
+        let charGridSize = this.getCharGridSize();
+        console.log(`getScreenSize`, this.getScreenSize());
+        console.log(`getCharGridSize`, charGridSize);
+        console.log(this.viewport.children);
+        console.log(this.screenDrawArea);
+        for (let y = 0; y < charGridSize.height; y++) {
+            for (let x = 0; x < charGridSize.width; x++) {
+                console.log(`x: ${x}, y: ${y}, index: ${y * charGridSize.width + x}`);
+                (this.viewport.children[y * charGridSize.width + x] as PIXI.Text).text = this.screenDrawArea[y * charGridSize.width + x]
             }
+        }
+    }
+
+    getWorldPontCanvasIndex(x: number, y: number) {
+        return y * this.getCharGridSize().width + x;
+    }
+
+    isWorldPointInScreen(x: number, y: number) {
+        return this.getCharGridSize().contains(x, y);
+    }
+
+    setWorldTable(table: Table) {
+        let rect = table.rect;
+        let headIndex = 0;
+        let firstColumnWidth = table.getColumnWidths()[0];
+        let secondColumnWidth = table.getColumnWidths()[1];
+        let thirdColumnWidth = table.getColumnWidths()[2];
+        this.paintWorldPatch8Safe(new Rectangle(rect.x, rect.y, rect.width, 2));
+        this.paintWorldPatch8Safe(new Rectangle(rect.x, rect.y + 2, firstColumnWidth, rect.height - 2));
+        this.paintWorldPatch8Safe(new Rectangle(rect.x + firstColumnWidth, rect.y + 2, secondColumnWidth, rect.height - 2));
+        this.paintWorldPatch8Safe(new Rectangle(rect.x + firstColumnWidth + secondColumnWidth, rect.y + 2, thirdColumnWidth, rect.height - 2));
+        for (let x = rect.x; x <= rect.right; x++) {
+            for (let y = rect.y; y <= rect.bottom; y++) {
+                if (! this.isWorldPointInScreen(x, y)) {
+                    continue;
+                }
+                if (rect.y + 1 === y && x < rect.right && x > rect.left) {
+                    let tile = table.head[headIndex] ?? ' ';
+                    headIndex++;
+                    this.screenDrawArea[this.getWorldPontCanvasIndex(x, y)] = tile;
+                }
+            }    
+        }
+    }
+
+    
+    paintWorldPatch8Safe(rect: Rectangle) {
+        let [tl, t, tr, ml, mr, bl, b, br] = ['+', '-', '+', '|', '|', '+', '-', '+'];
+        this.paintWorldPointToScreenSafe(rect.left, rect.top, tl);
+        this.paintWorldRectToScreenSafe(new Rectangle(rect.x + 1, rect.y, rect.width - 1, 1), t);
+        this.paintWorldPointToScreenSafe(rect.right, rect.top, tr);
+        this.paintWorldRectToScreenSafe(new Rectangle(rect.x, rect.y + 1, 1, rect.height - 1), ml);
+        this.paintWorldRectToScreenSafe(new Rectangle(rect.right, rect.y + 1, 1, rect.height - 1), mr);
+        this.paintWorldPointToScreenSafe(rect.left, rect.bottom, bl);
+        this.paintWorldRectToScreenSafe(new Rectangle(rect.x + 1, rect.bottom, rect.width - 1, 1), b);
+        this.paintWorldPointToScreenSafe(rect.right, rect.bottom, br);
+    }
+
+    paintWorldPointToScreenSafe(x: number, y: number, char: string) {
+        if (! this.isWorldPointInScreen(x, y)) {
+            return
+        }
+        this.screenDrawArea[this.getWorldPontCanvasIndex(x, y)] = char;
+    }
+
+    paintWorldRectToScreenSafe(rect: Rectangle, fillchar: string) {
+        for (let y = rect.y; y < rect.bottom; y++) {
+            for (let x = rect.x; x < rect.right; x++) {
+                this.paintWorldPointToScreenSafe(x, y, fillchar);
+            } 
         }
     }
 }

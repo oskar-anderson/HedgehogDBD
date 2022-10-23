bb787b8a1f8fc740d87dd649cf8c223a87fedd16
oskar-anderson <andersonkarloskar@gmail.com>
2022-10-23 21:35:50 +0300

    Updated: 2022-10-23T21:35:49

--- autocommit.js
+++ autocommit.js
@@ -2,6 +2,7 @@ import { exec } from "child_process";
 // .gitconfig 
 // [alias]
 //     xca = !git add . && git commit -m \"Updated: `date +\"%Y-%m-%dT%H:%M:%S\"`\"
+console.log("git xca")
 exec("git xca", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);

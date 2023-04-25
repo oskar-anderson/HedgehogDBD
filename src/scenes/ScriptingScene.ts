f9f7378b9b7310c69b429e60116de56975eb6fe8
oskar-anderson <andersonkarloskar@gmail.com>
2023-04-26 00:30:46 +0300

    add async scripting support

--- src/scenes/ScriptingScene.ts
+++ src/scenes/ScriptingScene.ts
@@ -23,9 +23,12 @@ export class ScriptingScene extends Container implements IScene {
         let SHARED = await fetch('src/wwwroot/scripts/_SHARED.js', {cache: "no-cache"}).then(x => x.text());
         let fnBody = `"use strict";\n${SHARED}\n${value}`;
         let schemaDTO = SchemaDTO.init(draw);
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/AsyncFunction
+        // https://stackoverflow.com/questions/46118496/asyncfunction-is-not-defined-yet-mdn-documents-its-usage
+        const AsyncFunction = async function () {}.constructor;
+        let fn = AsyncFunction("RESULT_LOG", "schema", "dayjs", "PIXI", fnBody);
         try {
-            let fn = Function("RESULT_LOG", "schema", "dayjs", "PIXI", fnBody);
-            fn(resultLog, schemaDTO, dayjs, PIXI);
+            await fn(resultLog, schemaDTO, dayjs, PIXI);
         } catch (error: any) {
             errorMsg = `${error.name}: ${error.message}`;
         }
@@ -38,8 +41,8 @@ export class ScriptingScene extends Container implements IScene {
     static async executeAndReturn(value: string, draw: Draw) {
         let SHARED = await fetch('src/wwwroot/scripts/_SHARED.js',  {cache: "no-cache"}).then(x => x.text());
         let fnBody = `"use strict";\n${SHARED}\n${value}`;
-        let schemaDTO = SchemaDTO.init(draw);
         let fn = Function("schema", fnBody);
+        let schemaDTO = SchemaDTO.init(draw);
         return fn(schemaDTO);
     }
 }

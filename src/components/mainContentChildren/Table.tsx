367d57b25d0a614d8525fd5c06ff9b7143b4ae41
oskar-anderson <andersonkarloskar@gmail.com>
2023-04-26 16:48:46 +0300

    Implement React table view

--- src/components/mainContentChildren/Table.tsx
+++ src/components/mainContentChildren/Table.tsx
@@ -6,33 +6,27 @@ import { Table } from "../../model/Table"
 import { TableRow } from "../../model/TableRow";
 import { DrawScene } from "../../scenes/DrawScene";
 import { TableScene } from "../../scenes/TableScene";
+import { useAppStateManagement } from "../../Store";
+import { AppState } from "../MainContent";
+import TableRowJSX from "./tableChildren/TableRow"
 
 
 
 export default function table() {
-
+    const { setAppState } = useAppStateManagement();
+    
     const tableBeingEdited = (Manager.getInstance().getScene() as TableScene).tableBeingEdited;
 
     const switchToDraw = () => {
         Manager.getInstance().changeScene(new DrawScene())
+        setAppState(AppState.DrawScene);
     }
 
-    const rowData: {
-        rowName: string, setRowName: React.Dispatch<React.SetStateAction<string>>,
-        rowDatatype: string, setRowDatatype: React.Dispatch<React.SetStateAction<string>>,
-        rowAttributes: string, setRowAttributes: React.Dispatch<React.SetStateAction<string>>
-    }[] = [];
-    const [trackedRowData, setTrackedRowData] = useState(rowData);
-    for(const row of tableBeingEdited.tableRows) {
-        const [rowName, setRowName] = useState(row.name);
-        const [rowDatatype, setRowDatatype] = useState(row.datatype);
-        const [rowAttributes, setRowAttributes] = useState(row.attributes.join(", "))
-        rowData.push({
-            rowName: rowName, setRowName: setRowName,
-            rowDatatype: rowDatatype, setRowDatatype: setRowDatatype,
-            rowAttributes: rowAttributes, setRowAttributes: setRowAttributes
-        })
-    }
+    const [rowData, setRowData] = useState(tableBeingEdited.tableRows.map((x) => { return {
+        rowName: x.name,
+        rowDatatype: x.datatype,
+        rowAttributes: x.attributes.join(", ")
+    } }));
 
     const [tableName, setTableName] = useState(tableBeingEdited.head);
 
@@ -41,7 +35,7 @@ export default function table() {
         let oldTable = draw.schema.tables.find(x => x.equals(tableBeingEdited))!;
         const newTable = Table.initClone(tableBeingEdited);
         newTable.head = tableName;
-        newTable.tableRows = trackedRowData.map(tableRow => TableRow.init(
+        newTable.tableRows = rowData.map(tableRow => TableRow.init(
             tableRow.rowName, 
             tableRow.rowDatatype, 
             tableRow.rowAttributes.split(",").map(x => x.trim())
@@ -56,46 +50,47 @@ export default function table() {
         ));
         draw.schema.relations.forEach(relation => relation.isDirty = true);
         Manager.getInstance().changeScene(new DrawScene())
+        setAppState(AppState.DrawScene);
     }
 
-    const changeRowNameField = (event: ChangeEvent, index: number) => {
-        rowData[index].setRowName((event.target as HTMLInputElement).value)
-    }
-
-    const changeRowDatatypeField = (event: ChangeEvent, index: number) => {
-        rowData[index].setRowDatatype((event.target as HTMLInputElement).value)
-    }
-
-    const changeRowAttributesField = (event: ChangeEvent, index: number) => {
-        rowData[index].setRowAttributes((event.target as HTMLInputElement).value)
+    const handleFormChange = (index: number, event: ChangeEvent, name: "rowName" | "rowDatatype" | "rowAttributes") => {
+        const data = [...rowData];
+        const value = (event.target as HTMLInputElement).value;
+        data[index][name] = value;
     }
 
     const insertNewRow = (event: FormEvent<HTMLButtonElement>, index: number) => {
-        if (index === -1) { index = trackedRowData.length}
-        const [rowName, setRowName] = useState("");
-        const [rowDatatype, setRowDatatype] = useState("");
-        const [rowAttributes, setRowAttributes] = useState("");
-        const newTrackedRows = [...trackedRowData].splice(index, 0, {
-            rowName: rowName, setRowName: setRowName,
-            rowDatatype: rowDatatype, setRowDatatype: setRowDatatype,
-            rowAttributes: rowAttributes, setRowAttributes: setRowAttributes,
-        });;
-        setTrackedRowData(newTrackedRows)
-        rowData[index].setRowAttributes((event.target as HTMLInputElement).value)
+        if (index === -1) { index = rowData.length}
+        const newRows = [
+            ...[...rowData].slice(0, index),
+            {
+                rowName: "",
+                rowDatatype: "",
+                rowAttributes: "",
+            },
+            ...[...rowData].slice(index)
+        ]
+        setRowData(newRows)
     }
 
     const moveRowUp = (index: number) => {
         if (index <= 0) return;
-        [trackedRowData[index], trackedRowData[index - 1]] = [trackedRowData[index - 1], trackedRowData[index]];
+        const newRows = [...rowData];
+        [newRows[index], newRows[index - 1]] = [newRows[index - 1], newRows[index]];
+        setRowData(newRows);
     }
 
     const moveRowDown = (index: number) => {
-        if (index >= trackedRowData.length - 1) return;
-        [trackedRowData[index], trackedRowData[index + 1]] = [trackedRowData[index + 1], trackedRowData[index]];
+        if (index >= rowData.length - 1) return;
+        const newRows = [...rowData];
+        [newRows[index], newRows[index + 1]] = [newRows[index + 1], newRows[index]];
+        setRowData(newRows);
     }
 
     const deleteRow = (index: number) => {
-        setTrackedRowData([...trackedRowData].splice(index, 1));
+        const newRows = [...rowData];
+        newRows.splice(index, 1);
+        setRowData(newRows);
     }
 
     const deleteTable = () => {
@@ -111,82 +106,69 @@ export default function table() {
         )
         draw.schema.relations.forEach(relation => relation.isDirty = true);
         Manager.getInstance().changeScene(new DrawScene())
+        setAppState(AppState.DrawScene);
     }
 
 
     return (
         <div className="table-edit-container">
-            <div className="modal" tabIndex={-1}>
+            <div className="modal" tabIndex={-1} style={{ display: "block" }}>
                 <div className="modal-dialog modal-dialog-scrollable" style={{maxWidth: "80%"}}>
-                    <div className="modal-header">
-                        <p className="modal-title">
-                            Table
-                            <input className="input-tablename" onChange={(e) => setTableName(e.target.value)} type="text" value={ tableName } />
-                        </p>
-                        <button type="button" className="btn-close" onClick={() => switchToDraw() } data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div className="modal-body">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th style={{ width: "120px" }}>Datatype</th>
-                                    <th>Attributes</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                { 
-                                    trackedRowData.map((row, index) => (
-                                        <tr key={index} data-index="{{ loop.index0 }}">
-                                            <td>
-                                                <input className="input-name" onChange={(e) => changeRowNameField(e, index)} type="text" value={ row.rowName } />
-                                            </td>
-                                            <td>
-                                                <input className="input-datatype" onChange={(e) => changeRowDatatypeField(e, index) } list="mysql-data-types" style={{width: "120px" }} value={ row.rowDatatype } />
-                                            </td>
-                                            <td>
-                                                <input className="input-attributes" onChange={(e) => changeRowAttributesField(e, index) } list="attribute-suggestions" type="text" value={ row.rowAttributes } />
-                                            </td>
-                                            <td>
-                                                <button className="row-insert-btn btn btn-primary" onClick={(e) => insertNewRow(e, index) }>Insert</button>
-                                                <button className="row-up-btn btn btn-primary" onClick={(e) => moveRowUp(index) }>Up</button>
-                                                <button className="row-down-btn btn btn-primary" onClick={(e) => moveRowDown(index) }>Down</button>
-                                                <button className="row-delete-btn btn btn-danger" onClick={(e) => deleteRow(index) }>Delete</button>
-                                            </td>
-                                        </tr>
-                                    ))
-                                }
-                                <tr data-index="{{ loop.index0 + 1 }}">
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td>
-                                        <button className="row-insert-btn btn btn-primary" onClick={(e) => insertNewRow(e, -1) }>Insert</button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <datalist id="mysql-data-types">
-                            <option value="VARCHAR(255)" />
-                            <option value="VARCHAR(255)?" />
-                            <option value="INT" />
-                            <option value="INT?" />
-                            <option value="FLOAT(14,2)" />
-                            <option value="FLOAT(14,2)?" />
-                            <option value="BOOLEAN" />
-                            <option value="BOOLEAN?" />
-                        </datalist>
-                        <datalist id="attribute-suggestions">
-                            <option value="PK" />
-                            <option value='FK("TableName")' />
-                        </datalist> 
-                    </div>
-                    <div className="modal-footer" style={{justifyContent: "space-between" }}>
-                        <button className="table-delete-btn btn btn-danger" onClick={() => deleteTable() }>Delete table</button>
-                        <div>
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" id="modal-save-changes" onClick={() => saveChanges() } className="btn btn-primary">Save changes</button>
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <p className="modal-title">
+                                <label htmlFor="table-name" style={{ marginRight: "6px" }}>Table</label>
+                                <input id="table-name" className="input-tablename" onChange={(e) => setTableName(e.target.value)} type="text" value={ tableName } />
+                            </p>
+                            <button type="button" className="btn-close" onClick={() => switchToDraw() } aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th style={{ width: "120px" }}>Datatype</th>
+                                        <th>Attributes</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    { 
+                                        rowData.map((row, index) => (
+                                            <TableRowJSX key={index} index={index} numberOfTableRows={rowData.length} row={row} handleFormChange={handleFormChange} insertNewRow={insertNewRow} moveRowUp={moveRowUp} moveRowDown={moveRowDown} deleteRow={deleteRow} />
+                                        ))
+                                    }
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td>
+                                            <button className="row-insert-btn btn btn-primary" onClick={(e) => insertNewRow(e, -1) }>Insert</button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <datalist id="mysql-data-types">
+                                <option value="VARCHAR(255)" />
+                                <option value="VARCHAR(255)?" />
+                                <option value="INT" />
+                                <option value="INT?" />
+                                <option value="FLOAT(14,2)" />
+                                <option value="FLOAT(14,2)?" />
+                                <option value="BOOLEAN" />
+                                <option value="BOOLEAN?" />
+                            </datalist>
+                            <datalist id="attribute-suggestions">
+                                <option value="PK" />
+                                <option value='FK("TableName")' />
+                            </datalist> 
+                        </div>
+                        <div className="modal-footer" style={{justifyContent: "space-between" }}>
+                            <button className="table-delete-btn btn btn-danger" onClick={() => deleteTable() }>Delete table</button>
+                            <div>
+                                <button type="button" className="btn btn-secondary" onClick={() => switchToDraw() }>Close</button>
+                                <button type="button" id="modal-save-changes" onClick={() => saveChanges() } className="btn btn-primary">Save changes</button>
+                            </div>
                         </div>
                     </div>
                 </div>

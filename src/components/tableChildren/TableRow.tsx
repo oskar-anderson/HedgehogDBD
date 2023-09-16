47f4bc078585d50d28d9c69e0253a2ce8e31418e
Karl Oskar Anderson <72770213+oskar-anderson@users.noreply.github.com>
2023-09-17 00:24:26 +0300

    Change SVG to match app style

--- src/components/tableChildren/TableRow.tsx
+++ src/components/tableChildren/TableRow.tsx
@@ -271,8 +271,10 @@ export default function TableRow({ index, hoverInsertIndicator, dragItem, dragOv
                     </button>
                     <OverlayTrigger trigger="click" placement="right" overlay={popover} rootClose={true}>
                         <button className={`btn btn-light btn-icon ${datatypeArguments.length === 0 ? "disabled" : ""}`} disabled={datatypeArguments.length === 0}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
+                            <svg width="24px" height="24px" viewBox="0 0 24 24" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17 12C17 12.5523 17.4477 13 18 13C18.5523 13 19 12.5523 19 12C19 11.4477 18.5523 11 18 11C17.4477 11 17 11.4477 17 12Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M11 12C11 12.5523 11.4477 13 12 13C12.5523 13 13 12.5523 13 12C13 11.4477 12.5523 11 12 11C11.4477 11 11 11.4477 11 12Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M5 12C5 12.5523 5.44772 13 6 13C6.55228 13 7 12.5523 7 12C7 11.4477 6.55228 11 6 11C5.44772 11 5 11.4477 5 12Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                             </svg>
                         </button>
                     </OverlayTrigger>

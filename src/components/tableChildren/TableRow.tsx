b072674b6aad19c3228b2a6a7b72355e5e2f8a55
Karl Oskar Anderson <72770213+oskar-anderson@users.noreply.github.com>
2023-09-26 14:31:22 +0300

    #23 Select hints using native select

--- src/components/tableChildren/TableRow.tsx
+++ src/components/tableChildren/TableRow.tsx
@@ -3,7 +3,6 @@ import { useApplicationState } from "../../Store"
 import DataType from "../../model/DataTypes/DataType"
 import Databases from "../../model/DataTypes/Databases"
 import { OverlayTrigger, Popover } from "react-bootstrap"
-import Select from 'react-select'
 import { useParams } from "react-router-dom"
 
 interface UiTableRowDatatype {
@@ -329,61 +328,6 @@ export default function TableRow({ index, hoverInsertIndicator, dragItem, dragOv
                         }
                     </optgroup>
                 </select>
-                <Select onChange={(e) => {
-                    let selected = e as any as { value: string, label: string }; // Type hint for onChange is broken for some reason
-                    let lenght = attributesTextInputField.current!.value.length
-                    let startPos = attributesTextInputField.current!.selectionStart ?? lenght;
-                    let endPos = attributesTextInputField.current!.selectionEnd ?? lenght;
-                    const newAttributeValue = 
-                        attributesTextInputField.current!.value.substring(0, startPos)
-                        + selected.value
-                        + attributesTextInputField.current!.value.substring(endPos, lenght);
-                    handleAttributeChange(newAttributeValue);
-                }} value={null}
-                    styles={{ 
-                        control: (baseStyles, state) => {
-                            console.log(state.isFocused);
-                            return {
-                                ...baseStyles,
-                                borderTopLeftRadius: 0,
-                                borderBottomLeftRadius: 0,
-                                // same focus color used by bootstrap or default
-                                borderColor: state.isFocused ? "#86b7fe" : "hsl(0, 0%, 80%)",
-                                border: "1px solid hsl(0, 0%, 80%)",
-                            }
-                        },
-                        menu: (baseStyles, state) => { 
-                            return {
-                                ...baseStyles,
-                                width: "160px",
-                            }
-                        },
-                        indicatorsContainer: (baseStyles, state) => { 
-                            return {
-                                ...baseStyles,
-                                display: "none"
-                            }
-                        }
-                    }}
-                    options={
-                    [
-                        {
-                            label: "Commands",
-                            options: [
-                                { value: `FK("tableName"), `, label: `FK(tableName)` },
-                                { value: "PK, ", label: "PK" }
-                            ]
-                        },
-                        {
-                            label: "Tables",
-                            options: tables
-                                .filter(table => table.id !== id)
-                                .map(table => (
-                                { value: table.head, label: table.head }
-                            ))
-                        }
-                    ]
-                } placeholder={"hints"}></Select>
             </td>
             <td>
                 <div style={{ display: "flex", flexWrap: "wrap", gap: "4px" }}>
